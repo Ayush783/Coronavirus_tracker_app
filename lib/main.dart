8469f6ba7a087286534e75badc848db0d77e4526
Ayush783 <ayushc45xxx@gmail.com>
2020-11-14 00:39:03 +0530

    setup auto route

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:corona_tracker/injection.dart';
+import 'package:corona_tracker/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
   configureInjection(Environment.prod);
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      title: 'Corona Tracker',
+      builder: ExtendedNavigator.builder(
+        router: Router(),
+      ),
+      theme: ThemeData.dark(),
+    ),
   );
 }
---
+++ lib/routes/router.dart
@@ -0,0 +1,7 @@
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:corona_tracker/ui/homescreen.dart';
+
+@MaterialAutoRouter(routes: <AutoRoute>[
+  MaterialRoute(page: HomeScreen, initial: true),
+])
+class $Router {}
