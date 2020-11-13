c2edb713e2573f23b145997e09363d03d7714cfb
Ayush783 <ayushc45xxx@gmail.com>
2020-11-13 19:20:10 +0530

    added required packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
-import 'package:coronavirus_tracker_app/repositories/dataRepo.dart';
-import 'package:coronavirus_tracker_app/screens/getDataScreen.dart';
-import 'package:coronavirus_tracker_app/services/APIService.dart';
-import 'package:coronavirus_tracker_app/services/api.dart';
 import 'package:flutter/material.dart';
-
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider<DataRepo>(
-      create: (_) => DataRepo(
-        apiService: APIService(
-          API.sandbox(),
-        ),
-      ),
-      child: MaterialApp(
-        theme: ThemeData(fontFamily: 'primary',
-        primaryColor: Color.fromRGBO(252, 99, 107, 1),
-        accentColor: Color.fromRGBO(252, 99, 107, 1),
-        ),
-        home: GetDataScreen(),
-        debugShowCheckedModeBanner: false,
-      ),
-    );
+    return Container();
   }
 }
